89025ff0ce2964c58c344943bb293eb824f9c7e8
saurabh01032003 <saurabh858182@gmail.com>
2024-05-09 13:50:23 +0530

    changes updated

--- 4. Javasvript/Lecture-17/Constructor Function/app.js
+++ 4. Javasvript/Lecture-17/Constructor Function/app.js
@@ -8,47 +8,69 @@
 
 // function sam(){}
 
-// let out = new sam(); // new key-word ki help se agar function call hua to -> it's called constructor function
-// console.log(out);
+// let obj = new sam(); // new key-word ki help se agar function call hua to -> it's called constructor function
+// console.log(obj);
 
 // -----------------------------------------------------
 
 // function Person() {
-//     this.naam = "Samarth";
+//     this.name = "Samarth";
 //     this.age = 33;
-// }
-// console.log
+// };
+
+// let person1 = new Person();
+// console.log(person1);
+// console.log(person1.naam)
 
 // // --------------------------------------
-// function User(naam, umar,sahi){
+// function User(naam, umar){
+
+//     // We are adding properties to object(i.e person1,person2), created by calling above function using "new" keyword
 //     this.name = naam;
 //     this.age = umar;
-//     this.isMale = sahi;
 //     this.dance = function(){
-//         console.log("Pappu cant dance");
+//         return `${this.name} cant dance`;
 //     };
 // }
 
-// let person1 = new User("Ronny",25, true)
-// let person2 = new User("Renuka",35, false)
+// let person1 = new User("Ronny",25)
+// let person2 = new User("Renuka",35)
 // console.log(person1);
 // console.log(person2);
+// // dance is function, So' it must be called
+// console.log(person1.dance());
+// console.log(person2.dance());
 
-// let obj = {};
-// new obj();
-
+// Constructor functions act as a blueprint to generate objects with simmilar properties
+//(i.e in above example person1 && person2 are two different objects but having same properties(name,age,dance), values may be diffrent (as here Ronny & Renuka are diffrent values corrosponding to property name));
 
 // -------------------------------------------------------
-// this
-function User(naam, umar,sahi){
+function User(naam, umar){
     this.name = naam;
     this.age = umar;
-    this.isMale = sahi;
 }
 
-User.prototype.dance = function (){
-    console.log("Pappu cna't dance saala")
-};
+// Hamne User ke prototype me dance naam ki property insert kardi, now you can access it 
 
-let person1 = new User("Ronny",25,true);
+User.prototype.dance = function (){
+    console.log(`${this.name} cna't dance saala`)
+};
+let person1 = new User("Ronny",25);
+let person2 = new User("Renuka",23);
+
+// User itself a prototype
+console.log(person1);
 person1.dance();
+                                                                
+console.log(User.prototype.__proto__ === Object.prototype); // User.__proto__ : shows us a funcion
+
+// // // User is itself a prototype and acting as fallback source for object created
+console.log(User.__proto__ === Function.prototype)
+
+console.log(Function.prototype.__proto__ === Object.prototype);
+console.log(User.prototype)
+
+// // console.log(person1.prototype);
+// console.log(person2.__proto__); // dance property is stored
+// console.log(person1.__proto__ === User.prototype);
+
