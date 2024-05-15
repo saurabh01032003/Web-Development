d6e88a3df97b703817676da39191aa94967db251
saurabh01032003 <saurabh858182@gmail.com>
2024-05-15 07:06:57 +0530

    till arrow function done

--- 4. Javasvript/Lecture-18/Arrow-functions/app.js
+++ 4. Javasvript/Lecture-18/Arrow-functions/app.js
@@ -1,60 +1,55 @@
-// Cube
+// // Cube
 // function cube(a){
 //     return a**3
 // }
 
-// Arrow function for above 
-// let cube = a => a*a*a;
+// // Arrow function for above 
+
+// // let cube = a => a*a*a;
 
 // console.log(cube(3));
+
+//-------------------------
+// function area(length,breadth){
+//     return length*breadth;
+// }
+
+// console.log(area);
+// console.log(area(4,5));
+
+// // Arrow function for above function is
 
 // let area = (length,breadth)=> length*breadth;
 
 // console.log(`Area of Rectangle is ` + area(2,3));
 
-// Arrow function ka "this" is dependent on parent ka "this" pointer
+// //----------------------------------
+// function square(n){
+//     return n*n;
+// }
+// console.log(square(4));
 
-// let obj = {
-//     a:10,
-//     fn: ()=>{
-//         console.log(this);
-//         function sam(){
-//             console.log(this);
-//         }
-//         sam();
-//     },
-// };
-// obj.fn();
+// //----------- way-1
+// let square = (n) => {
+//     return n*n;
+// }
+// console.log(square(4));
 
-// ------------------------------------------
-// let obj1 = {
-//     a: "Saurabh",
-//     fn: function(d,e,f){
-//         console.log(`hello from ${this.a} `,d,e,f);
-//     },
-// };
 
-// let obj2 = {
-//     a: "Paras",
-// };
+// //----------- way-2
+// let square = (n) => n*n; // no need of return statement if only one line is present in body and that statement is return statement
+// console.log(square(4));
 
-// // .call() && .apply() used to change reference of this key-word
-// obj1.fn(10,20,30);
-// obj1.fn.call(obj2,10,20,30);
-// // obj1.fn.apply(obj2,10,20,30); // error
-// obj1.fn.apply(obj2,[10,20,30]); // arguements with array must
+// // ---------- way-3
+
+// let square = n => n*n; // no need to pass parenthesis if there is only one arguement in function
+// console.log(square(4));
+
 
 // =========================================================
-// let obj = {
-//     num: 3,
-//     fn: function () {
-//         console.log(this);
-//     }
-// }
-// obj.fn();
-
 
 //5. arrow function -> ka this always points to parent ka this
+
 // let obj = {     //this --> window
 //     num: 3 , 
 //     fn: ()=>{
@@ -65,14 +60,29 @@
 
 
 // question:
-let obj = {
-    num: 30,
-    fn: function () {
-        let calculate = () => {
-            console.log(this) //parent ke this ko
-        }
-        calculate();
-    }
-}
+// let obj = {
+//     num: 30,
+//     fn: function () {
+//         console.log(this);
+//         let calculate = () => {
+//             console.log(this) //parent ke this ko
+//         }
+//         calculate();
+//     }
+// }
 
-obj.fn(); //method calling
+// obj.fn(); //method calling
+
+//--------------- Arrow function ka "this" is dependent on parent ka "this" pointer
+
+let obj = {
+    a:10,
+    fn: ()=>{
+        console.log(this);
+        function sam(){
+            console.log(this);
+        }
+        sam();
+    },
+};
+obj.fn();
