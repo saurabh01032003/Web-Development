bd5c71b33c9654ca5178bc48b877d73e24256e07
saurabh01032003 <saurabh858182@gmail.com>
2024-05-28 11:00:35 +0530

    Revised till Lecture-7 again

---
+++ 6. AJAX/API Calling/app.js
@@ -0,0 +1,50 @@
+// -------------Method 1 : 
+
+// const req = new XMLHttpRequest()
+
+
+// // console.log(req);
+
+// // console.log(req.onload);
+// // console.log(req.error);
+
+// // ------------------------
+
+// req.open('GET','https://api.tvmaze.com/singlesearch/shows?q=girls'); // to create a link between client and endpoint (think client as remote && endpoint in server)
+// req.send(); // remote ka button daba diya
+
+// // console.log(req);
+
+// // onerror is property of object req, but here it is acting like eventListener
+// req.onerror = function(res){ // api ki data aayi hai in arguement, jo send hui thi pahle wo aayi
+//     console.log(res,"error");
+// }
+
+// req.onload = function(res){ 
+//     console.log(res,"load");
+// }
+
+// ------------------------2. fetch -> webapi -> returns a promise 
+
+// let res = fetch('https://api.tvmaze.com/singlesearch/shows?q=batman');
+
+// // console.log(res);
+
+// res.then((function(data){
+//     console.log(data);
+//     console.log(data.json());
+// })).catch(
+    
+// )
+
+
+// ------------------------------3. Axios
+
+let req = axios.get('https://api.tvmaze.com/singlesearch/shows?q=batman'); // returns promise
+
+req.then(function(res){
+    console.log(res.data); // we get complete data not like fetch which gives us meta data 
+})
+.catch(function(err){
+    console.log("err");
+})
