9d23b5e4344fb5f2d57d63226b7298b7bcd8818a
saurabh01032003 <saurabh858182@gmail.com>
2024-07-02 19:23:46 +0530

    Done till Project

---
+++ 6. AJAX and API/Lecture-30/Movie Searching APP/app.js
@@ -0,0 +1,38 @@
+let input = document.querySelector('input');
+let list = document.querySelector('#list');
+let btn = document.querySelector('button');
+
+btn.addEventListener('click',function(){
+    let searchText = input.value;
+    console.log(searchText);
+    getData(searchText);
+})
+function getData(searchText){
+    let res = axios.get(`https://api.tvmaze.com/search/shows?q=${searchText}`);
+    res.then(function(response){
+        console.log(response);
+        // console.log(response.data);
+        manipulationDom(response.data);
+    }).catch(function(err){
+        console.log("Error");
+    })
+}
+
+function manipulationDom(allData){
+
+    // remove previos search histories
+    while(list.firstChild){
+        list.firstChild.remove();
+    }
+
+    console.log(allData);
+    for(let item of allData){
+        let figure = document.createElement('figure');
+        figure.innerHTML = `
+            <img src=${item.show.image.original} />
+            <h2>${item.show.name}</h2>    
+        `
+        list.append(figure);
+    }
+
+}
